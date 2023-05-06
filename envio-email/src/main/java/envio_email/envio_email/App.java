eefea9e2d1c86e093440412c1ff6c265da85dba1
victorbaydir <victorbaydir@hotmail.com>
2023-05-06 18:37:47 -0300

    projeto envio email - 06-05-2023

---
+++ envio-email/src/main/java/envio_email/envio_email/App.java
@@ -0,0 +1,13 @@
+package envio_email.envio_email;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
